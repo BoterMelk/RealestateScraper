5697230e516757e4c88d2069905079f73408755c
Loise van Dijk <loisedijk@gmail.com>
2022-11-29 15:44:50 +0100

    removed selenium import. put code into function

--- sitescraper.py
+++ sitescraper.py
@@ -1,28 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
-from selenium import webdriver
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
 
+def itshousing():
+    url="https://itshousing.nl/woningaanbod/huur"
+    page = requests.get(url)
 
-url="https://itshousing.nl/woningaanbod/huur"
-page = requests.get(url)
+    pageContent = BeautifulSoup(page.content, 'html.parser')
+    listings = pageContent.find_all('article', class_='objectcontainer')
+    itshousingList = []
 
-pageContent = BeautifulSoup(page.content, 'html.parser')
-listings = pageContent.find_all('article', class_='objectcontainer')
-itshousingList = []
-
-for list in listings:
-    listingTitle = list.find('span', class_="street").text
-    listingLocation = list.find('span', class_="location").text
-    listingPrice = list.find('span', class_="obj_price").text
-    listingPrice = listingPrice.replace("\t", "").replace("\r", "").replace("\n", "")
-    listingPrice = listingPrice.strip()
+    for list in listings:
+        listingTitle = list.find('span', class_="street").text
+        listingLocation = list.find('span', class_="location").text
+        listingPrice = list.find('span', class_="obj_price").text
+        listingPrice = listingPrice.replace("\t", "").replace("\r", "").replace("\n", "")
+        listingPrice = listingPrice.strip()
     
-    if listingLocation == "Utrecht":
-        ListingInfo = [listingTitle, listingLocation, listingPrice]
-        itshousingList.append(ListingInfo)
+        if listingLocation == "Utrecht":
+            ListingInfo = [listingTitle, listingLocation, listingPrice]
+            itshousingList.append(ListingInfo)
+        
+    return(itshousingList)
 
-print(itshousingList)
+
+
+print(itshousing()) 
+allResults = []
+allResults = allResults.append(itshousing())
+print("test")
+print(allResults)
